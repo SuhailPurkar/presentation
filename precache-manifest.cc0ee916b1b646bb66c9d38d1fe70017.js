7408299af88259dc8ba858b0bfa4382c413e9b48
suhailpurkar <suhailpurkar@gmail.com>
2019-11-22 04:31:32 -0500

    Updates

---
+++ precache-manifest.cc0ee916b1b646bb66c9d38d1fe70017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10d3df0f24af471496272bd987706ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e95b6269624f0a24dd5",
+    "url": "/static/css/main.e2f1f95d.chunk.css"
+  },
+  {
+    "revision": "e43118d6f554f6b1272b",
+    "url": "/static/js/2.a3d23936.chunk.js"
+  },
+  {
+    "revision": "9e95b6269624f0a24dd5",
+    "url": "/static/js/main.428353dd.chunk.js"
+  },
+  {
+    "revision": "45ab5480b8a491574cd3",
+    "url": "/static/js/runtime-main.7b7b1f81.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
